f145bb24df4a8bee58c8d064467fef61267470f0
Robert Evans <rmevans1@coastal.edu>
2022-02-14 11:01:34 -0500

    Tickets admin

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Ticket, Reservation
 
 # Register your models here.
+class TicketInline(admin.StackedInline):
+    model = Ticket
+    extra = 2
+
+class ReservationAdmin(admin.ModelAdmin):
+    fields = ['flight', 'num_people', 'total_cost']
+    inlines = ['TicketInline']
+
+admin.site.register(Reservation, ReservationAdmin)
