976573a1d65eccbcef3b3795a2b299751323b7ec
Robert Evans <rmevans1@coastal.edu>
2022-02-14 09:41:12 -0500

    Airports admin

--- airports/admin.py
+++ airports/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Airport, Runway
 
 # Register your models here.
+class AirportAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'airport_code', 'is_open']}),
+        ('Airport Address', {'fields': ['address', 'city', 'state', 'zipcode'], 'classes': ['collapse']})
+    ]
+
+admin.site.register(Airport, AirportAdmin)
