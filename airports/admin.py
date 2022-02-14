39757745e0ef1e03084924c6d0938b2fb262c455
Robert Evans <rmevans1@coastal.edu>
2022-02-14 09:53:24 -0500

    Airports admin

--- airports/admin.py
+++ airports/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from .models import Airport, Runway
 
 # Register your models here.
+class RunwayInline(admin.StackedInline):
+    model = Runway  # Specify which model to use
+    extra = 2  # How many to start with
+
 class AirportAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['name', 'airport_code', 'is_open']}),
         ('Airport Address', {'fields': ['address', 'city', 'state', 'zipcode'], 'classes': ['collapse']})
     ]
+    inlines = [RunwayInline]  # Load the RunwayInLine Class
 
 admin.site.register(Airport, AirportAdmin)
