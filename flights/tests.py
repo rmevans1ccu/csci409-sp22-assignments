71692c2c2b5c0728cd881e2ff7397b99f2998a87
Robert Evans <rmevans1@coastal.edu>
2022-02-14 09:04:39 -0500

    FLights Model Test

--- flights/tests.py
+++ flights/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.test import Client
 from airports.models import Airport, Runway
+from flights.models import Flight
+from datetime import datetime
 
 
 class FlightTestCase(TestCase):
@@ -13,6 +15,12 @@ class FlightTestCase(TestCase):
         a2 = Airport.objects.create(name="Hartsfield - Jackson International Airport", airport_code="ATL",
                                     address="1100 Jet Port Road", city="Myrtle Beach",
                                     state="SC", zipcode="29577", is_open=True)
+        Flight.objects.create(origin=a1, destination=a2,
+                              departure=datetime.now(), arrival=datetime.now(),
+                              aircraft_type="CRJ9")
+        Flight.objects.create(origin=a2, destination=a1,
+                              departure=datetime.now(), arrival=datetime.now(),
+                              aircraft_type="B747")
         Runway(runway_number=18, runway_designation="N", length=9503, width=150, airport=a1).save()
         Runway(runway_number=36, runway_designation="N", length=9503, width=150, airport=a1).save()
         self.client = Client()
