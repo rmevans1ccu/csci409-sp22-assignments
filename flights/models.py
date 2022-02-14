c61391d58baa7551fe15c1fe43505a764ab275b2
Robert Evans <rmevans1@coastal.edu>
2022-02-14 10:41:19 -0500

    Add flight number

--- flights/models.py
+++ flights/models.py
@@ -9,6 +9,8 @@ class Airline(models.Model):
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name='flight_origin')
     destination = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name='flight_destination')
+    airline = models.ForeignKey(Airline, on_delete=models.PROTECT)
+    flight_number = models.IntegerField()
     departure = models.DateTimeField()
     arrival = models.DateTimeField()
     aircraft_type = models.CharField(max_length=10)
