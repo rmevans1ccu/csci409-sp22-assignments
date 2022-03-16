2331320874852a9eda03cc9a2dfbf1a304834acc
Robert Evans <rmevans1@coastal.edu>
2022-03-16 16:15:35 -0400

    Reservation view

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from .models import Reservation
 
 def index(request):
+    # Nothing to do here
     return HttpResponse('Hello from tickets')
 
 def ticket_search(request, confirmation_number):
-    return HttpResponse('Search for tickets for confirmation number: ' + str(confirmation_number))
+    # Select the singular reservation for the confirmation number
+    # Note: the confirmation_number is the id in the Reservation table
+    reservation = Reservation.objects.get(id=confirmation_number)
+    return HttpResponse('Number of tickets for confirmation number: ' + str(confirmation_number) + " is " + str(reservation.num_people))
