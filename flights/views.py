9d8c7b97501c9d73e661e899b0da922389828fe1
Robert Evans <rmevans1@coastal.edu>
2022-03-16 16:01:28 -0400

    Finish flight view

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from .models import Flight
+from airports.models import Airport
 
 def index(request):
-    return HttpResponse('Hello from flights')
+    flights = Flight.objects.all()
+    flight_list = ', '.join([f.origin.airport_code + "->" + f.destination.airport_code for f in flights])
+    return HttpResponse('Listing all flights: ' + flight_list)
 
 def flight_search(request, origin, destination):
-    return HttpResponse('Showing flights from ' + origin + ' to ' + destination)
+    origin = Airport.objects.get(airport_code=origin)
+    destination = Airport.objects.get(airport_code=destination)
+    flights = Flight.objects.filter(origin=origin.id, destination=destination.id)
+    flight_list = ', '.join([f.origin.airport_code + "->" + f.destination.airport_code + " Airline Code: " + f.airline.airline_code for f in flights])
+    return HttpResponse('Showing flights: ' + flight_list)
