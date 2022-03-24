8e31da0f18234c8c31027c261a6c0f4739a118f1
Robert Evans <rmevans1@coastal.edu>
2022-03-24 10:58:12 -0400

    Airport index view

--- airports/views.py
+++ airports/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render # Import the render library to make loading templates eaiser
 from .models import Airport
 
 def index(request):
     # Fetch all airports from database
     airports = Airport.objects.all()
-    # Create a displayable string. We will change this next week.
-    # This is just to show some data.
-    airport_list = ', '.join([a.airport_code for a in airports])
-    return HttpResponse('Showing all airports: ' + airport_list);
+    # Place all airports into a context variable for retrieval in the view.
+    context = {'airports': airports}
+    return render(request, 'airports/index.html', context)
 
 def airport_info(request, airport_code):
     # Fetch the airport by a certain code
